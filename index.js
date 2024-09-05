d95f9bb951817f728550c73bd2b235284dfe2df3
Artem Vasilev <kern.usr@gmail.com>
2024-09-05 20:19:17 +0300

    Add "required" option for all optional inputs

--- index.js
+++ index.js
@@ -7,77 +7,80 @@ import moment from 'moment/min/moment-with-locales';
 const WEEK = 7;
 
 function capitalize(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
+    return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 function addDate(date, count) {
-  const result = new Date(date);
-  result.setDate(result.getDate() + count);
-  return result;
+    const result = new Date(date);
+    result.setDate(result.getDate() + count);
+    return result;
 }
 
 async function run() {
-  const token = core.getInput('token');
-  const octokit = github.getOctokit(token);
+    const token = core.getInput('token');
+    const octokit = github.getOctokit(token);
 
     const baseTitle = core.getInput('title', {trimWhitespace: false});
     const days = core.getInput('days').split(',');
 
-  const count = core.getInput('count');
-  const format = core.getInput('format');
-  const locale = core.getInput('locale');
+    // eslint-disable-next-line try-catch-failsafe/json-parse
+    const dateOpts = core.getInput('date_options', {required: false}) && JSON.parse(core.getInput('date_options', {required: false}));
 
-  const promises = [];
+    const count = core.getInput('count', {required: false}) || 1;
+    const format = core.getInput('format', {required: false});
+    const locale = core.getInput('locale', {required: false});
 
-  for (let i = 0; i < count; i += 1) {
-    const d = addDate(new Date(), WEEK * i);
-    d.setHours(23);
-    d.setMinutes(59);
-    d.setSeconds(59);
+    const promises = [];
 
-    days.forEach(day => {
-      promises.push(
-        new Promise((resolve, reject) => {
-          const date = datefns[`next${capitalize(day)}`](d);
+    for (let i = 0; i < count; i += 1) {
+        const d = addDate(new Date(), WEEK * i);
+        d.setHours(23);
+        d.setMinutes(59);
+        d.setSeconds(59);
 
-          let title = baseTitle;
-          if (dateOpts) {
-            title += ` (${date.toLocaleDateString(locale || undefined, dateOpts)})`;
-          } else {
-            title += moment(date).locale(parse(locale).language).format(format);
-          }
+        days.forEach(day => {
+            promises.push(
+                new Promise((resolve, reject) => {
+                    const date = datefns[`next${capitalize(day)}`](d);
 
-          octokit.rest.issues
-            .createMilestone({
-              ...github.context.repo,
-              title,
-              due_on: date,
-            })
-            .then(({ data }) => {
-              resolve(data.html_url);
-            })
-            .catch(err => {
-              // If the milestone already exists `err.message` will look like the following:
-              //    'Validation Failed: {"resource":"Milestone","code":"already_exists","field":"title"}'
-              if (!err.message.match(/already_exists/)) {
-                reject(err);
-              }
-            });
-        })
-      );
+                    let title = baseTitle;
+                    if (dateOpts) {
+                        title += ` (${date.toLocaleDateString(locale || undefined, dateOpts)})`;
+                    } else {
+                        title += moment(date).locale(parse(locale).language).format(format);
+                    }
+
+                    octokit.rest.issues
+                        .createMilestone({
+                            ...github.context.repo,
+                            title,
+                            due_on: date,
+                        })
+                        .then(({data}) => {
+                            resolve(data.html_url);
+                        })
+                        .catch(err => {
+                            // If the milestone already exists `err.message` will look like the following:
+                            //    'Validation Failed: {"resource":"Milestone","code":"already_exists","field":"title"}'
+                            if (!err.message.match(/already_exists/)) {
+                                reject(err);
+                            }
+                        });
+                })
+            );
+        });
+    }
+
+    const milestones = [];
+    await Promise.all(promises).then(url => {
+        milestones.push(url);
     });
-  }
 
-  const milestones = [];
-  await Promise.all(promises).then(url => {
-    milestones.push(url);
-  });
-
-  core.setOutput('milestones', milestones);
+    core.setOutput('milestones', milestones);
 }
 
 try {
-  run();
+    run();
 } catch (err) {
-  core.setFailed(err.message);
+    core.setFailed(err.message);
 }
